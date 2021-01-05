bb6027967c84e868a66c346431f6116037195661
sandy1110 <sandiig.1110@gmail.com>
2021-01-04 20:53:42 -0600

    agregamos conexion a base de datos

--- index.js
+++ index.js
@@ -2,7 +2,23 @@
 const app = require("./app");
 const ip = require("ip");
 const port = 8080;
+const mongoose = require('mongoose');
 
-app.listen(port, () => {
-    console.log(`API REST corriendo en http://${ip.address()}:${port}/`)
-});
+const connectionOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}
+
+mongoose.connect("mongodb+srv://dev_user:NzuR5EGLBByKvfCj@cluster0.2vlym.mongodb.net/dev?retryWrites=true&w=majority",
+connectionOptions, (error, res) => {
+    if(error){
+        return console.log("no se pudo conectar", error);
+    }else{
+        console.log ("Nos conectamos a la base de datos");
+        app.listen(port, () => {
+            console.log(`API REST corriendo en http://${ip.address()}:${port}/`)
+        });
+    }
+})
